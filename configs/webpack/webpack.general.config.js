8f0e87a7adea968b0857f1cd9ee88c294ea35594
kritzona <virell.m@yandex.ru>
2021-04-12 15:14:17 +0300

    Установил парсер HTML-шаблонов

--- configs/webpack/webpack.general.config.js
+++ configs/webpack/webpack.general.config.js
@@ -1,11 +1,24 @@
 const path = require('path')
-const { paths } = require('../main/config')
 const webpack = require('webpack')
+const { paths } = require('../main/config')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(paths.sourceDirPath, './main.ts'),
   output: {
     path: paths.distDirPath,
     filename: '[name].[contenthash].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack-конфигурация для React-приложений',
+      template: path.resolve(paths.publicDirPath, './index.html'),
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [],
   },
 }
