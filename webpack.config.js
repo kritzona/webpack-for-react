744551ede3fdf2a73a9ceeb348ff8ba6895fae76
kritzona <virell.m@yandex.ru>
2021-04-12 14:46:33 +0300

    Разнес части конфигов, кроме корневого, в отдельную папку

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 
-const webpackGeneralConfig = require('./webpack.general.config')
-const webpackDevConfig = require('./webpack.dev.config')
-const webpackProdConfig = require('./webpack.prod.config')
+const webpackGeneralConfig = require('./configs/webpack/webpack.general.config')
+const webpackDevConfig = require('./configs/webpack/webpack.dev.config')
+const webpackProdConfig = require('./configs/webpack/webpack.prod.config')
 
 module.exports = (env) => {
   const isWebpackBuild = !!env.WEBPACK_BUILD
