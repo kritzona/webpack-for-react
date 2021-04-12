6ce2b8606ae878342fad7deaecee68f898304d5d
kritzona <virell.m@yandex.ru>
2021-04-12 14:44:16 +0300

    Сделал красивое слияние

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,10 @@ const webpackDevConfig = require('./webpack.dev.config')
 const webpackProdConfig = require('./webpack.prod.config')
 
 module.exports = (env) => {
-  switch (env) {
-    case 'development':
-      console.log(env)
-      return merge(webpackGeneralConfig, webpackDevConfig)
-    case 'production':
-      console.log(env)
-      return merge(webpackGeneralConfig, webpackProdConfig)
-    default:
-      console.log(env)
-      return merge(webpackGeneralConfig, webpackProdConfig)
-  }
+  const isWebpackBuild = !!env.WEBPACK_BUILD
+
+  merge(
+    webpackGeneralConfig,
+    isWebpackBuild ? webpackProdConfig : webpackDevConfig,
+  )
 }
